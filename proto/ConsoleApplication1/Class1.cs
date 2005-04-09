096b08afc37c88fd78f65a51442c15c6c211677c
Миша <@localhost>
2005-04-09 13:42:30 +0000

    Temporary file created by Visual Studio .NET to detect Microsoft Visual SourceSafe capabilities.

---
+++ proto/ConsoleApplication1/Class1.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApplication1
+{
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			//
+			// TODO: Add code to start application here
+			//
+			Console.WriteLine("8!=~" + Math.Sqrt(Math.PI)/Math.Exp(8)*Math.Pow(8,7)*97/3);
+			Console.ReadLine();
+		}
+	}
+}
